a6b658a9ee625e788cbbd2a96a655cfc377cb28c
prathamesh2304 <prathameshthakur.engg@gmail.com>
2024-10-19 12:13:17 +0530

    updated

--- ChaiCodeJavaScript/D5String.js
+++ ChaiCodeJavaScript/D5String.js
@@ -1,5 +1,20 @@
 const name = "Prathamesh"
-const repoCount =  45467
+const rollCount =  45467
 
-console.log(name + repoCount + "Value");
-                
+// console.log( name," " + repoCount," " + "Value"); 
+// *************************Above way of printing is very wrong***********************************
+
+console.log(`My Name Is ${name} & My RepoCount Is ${rollCount}.`);
+            
+const stdName = new String('Prajwal')
+    console.log(stdName);
+    console.log(stdName.charAt(0))
+    console.log(stdName.toLowerCase());
+    console.log(stdName.toUpperCase());
+    
+    
+    // 1. Write;
+    // 2. Youtube;
+    // 3. Setup
+
+    
