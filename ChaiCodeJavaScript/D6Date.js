66f144c00a91fdaf9340e7f5184e189517ff5ed8
Prathamesh Punamchand Thakur <prathameshthakur.official@gmail.com>
2024-11-11 18:10:13 +0000

    Date

---
+++ ChaiCodeJavaScript/D6Date.js
@@ -0,0 +1,22 @@
+/*
+ let myDate = new Date()
+console.log(myDate.toString()) 
+  o/p -> Mon Nov 11 2024 16:31:38 GMT+0000
+console.log(myDate.toISOString()) 
+  o/p -> 2024-11-11T16:33:49.580Z
+console.log(myDate.toLocaleDateString())
+  o/p -> 11/11/2024  
+console.log(typeof(myDate))
+  o/p -> object
+*/
+
+/*
+let myNewDate = new Date(2023,5,8) 
+  -> Thu Jun 08 2023 00:00:00 GMT+0000 
+let myNewDate = new Date(2023,5,8,6,8,59)
+  -> 6/8/2023, 6:08:59 AM 
+let myNewDate = new Date("2023-5-8") 
+  -> YYYY-DD-MM Format
+let myNewDate = new Date("8-5-2023") 
+  ->  DD-MM-YYYY Format
+*/
