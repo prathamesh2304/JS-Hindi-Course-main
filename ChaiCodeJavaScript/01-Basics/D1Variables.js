872276179a8c1fa5966d01b897fd44dadab58a8a
prathamesh2304 <prathameshthakur.engg@gmail.com>
2025-02-14 20:20:23 +0530

    new folder

---
+++ ChaiCodeJavaScript/01-Basics/D1Variables.js
@@ -0,0 +1,14 @@
+const accountNumber = 12345
+var accountName = "Prathamesh"
+let accountEmail = "prathameshthakur9421@gmail.com"
+accountCity = "Chh sambhajinagar"
+
+// accountNumber = 4321 -> Const Variable Value Can't Changeable In Whole Code
+// Prefered To Use Let Instead Var Coz Of Block Scope Or Functional Scope 
+ 
+accountName = "Aniket"
+accountEmail = "aniket9421@gmail.com"
+accountCity = "Jaipur"
+ 
+console.log(accountNumber);
+console.table([accountName,accountEmail,accountCity])
---
+++ ChaiCodeJavaScript/01-Basics/D5String.js
@@ -0,0 +1,20 @@
+const name = "Prathamesh"
+const rollCount =  45467
+
+// console.log( name," " + repoCount," " + "Value"); 
+// *************************Above way of printing is very wrong***********************************
+
+console.log(`My Name Is ${name} & My RepoCount Is ${rollCount}.`);
+            
+const stdName = new String('Prajwal')
+    console.log(stdName);
+    console.log(stdName.charAt(0))
+    console.log(stdName.toLowerCase());
+    console.log(stdName.toUpperCase());
+    
+    
+    // 1. Write;
+    // 2. Youtube; 
+    // 3. Setup
+
+    
