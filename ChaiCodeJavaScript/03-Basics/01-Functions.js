1ed49fe54de1a9bc1e7c003df853d3727b87d43c
prathamesh2304 <prathameshthakur.engg@gmail.com>
2025-03-02 08:53:33 +0530

    function and object

--- ChaiCodeJavaScript/03-Basics/01-Functions.js
+++ ChaiCodeJavaScript/03-Basics/01-Functions.js
@@ -32,3 +32,45 @@ function loginUserMessage(username){
 }
 
 console.log(loginUserMessage("Prathamesh"));
+
+function calculateCartPrice(...num1){ // rest operator similar as to spread operator differentiated according to use
+    
+        return num1
+
+    }
+console.log(calculateCartPrice(100,200,300)); // [ 100, 200, 300 ]
+
+function calculateCartPrice2(val1,val2,...num1){ // rest operator similar as to spread operator differentiated according to use
+    
+    return num1
+
+}
+console.log(calculateCartPrice2(500,800,400,2000)); // num1 will hold this = [ 400, 2000 ] remainning in val1 and val2
+
+const user = {
+    username: "Prathamesh",
+    price: 199,
+}
+
+    function handleObject(anyObject){
+       
+        console.log(`Username is ${anyObject.username} Price is ${anyObject.price}`);
+        
+    }
+    // handleObject(user)
+    handleObject(
+        {
+    username:"Aniket",
+    price: 899
+        }
+)
+
+const myNewArray = [100,200,300]
+
+function returnSecondValue(anyArray){
+    console.log(anyArray[1]);
+    
+}
+
+// returnSecondValue(myNewArray)
+returnSecondValue([500,6000,800]) // 6000
