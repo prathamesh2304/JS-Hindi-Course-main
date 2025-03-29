b1241bd9f777ae5aefc8edb44716e91f9bce07f7
prathamesh2304 <prathameshthakur.engg@gmail.com>
2025-03-30 01:16:12 +0530

    Static Property

---
+++ ChaiCodeJavaScript/09-Classes&OOPs/07-StaticProp.js
@@ -0,0 +1,32 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNSME : ${this.username}`);
+        
+    }
+
+    // static used for this methos those who we want to don't have accese to user in some cases like below
+    static createId(){
+        return '9843985'
+    }
+}
+
+const user = new User("Prathamesh")
+// Before static 
+// console.log(user.createId()) // 9843985
+// After static
+// console.log(user.createId()) // TypeError: user.createId is not a function
+
+class Teacher extends User{
+    constructor(username,email){
+        super(username)
+        this.email = email;
+    }
+}
+
+const iphone = new Teacher("iphone","iphone@google.com")
+// ---> iphone.logMe() // USERNSME : iphone
+console.log(iphone.createId()) // TypeError: iphone.createId is not a function
